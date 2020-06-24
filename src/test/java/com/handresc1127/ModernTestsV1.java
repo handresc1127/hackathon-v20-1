301cc7023d78c9245a1f7a0c3e21cb7f9dcd6e71
handresc1127 <h.andresc1127@gmail.com>
2020-06-23 21:52:17 -0500

    Moder Test task1

--- src/test/java/com/handresc1127/ModernTestsV1.java
+++ src/test/java/com/handresc1127/ModernTestsV1.java
@@ -1,4 +1,24 @@
 package com.handresc1127;
 
-public class ModernTestsV1 {
+import com.handresc1127.pages.homePage;
+import com.handresc1127.utils.BaseTests;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class ModernTestsV1 extends BaseTests {
+
+    @BeforeClass
+    public static void startVisualTestSuite(){
+        eyesManager.setBatchName("UFG Hackathon");
+        eyesManager.setForceFullScreen(true);
+    }
+
+    @Test
+    public void task1()
+    {
+        homePage.navigateToV2();
+        eyesManager.validateWindows("Cross-Device Elements Test");
+    }
+
+
 }
--- src/test/java/com/handresc1127/TraditionalTestsV1/Task1.java
+++ src/test/java/com/handresc1127/TraditionalTestsV1/Task1.java
@@ -1,11 +1,10 @@
 package com.handresc1127.TraditionalTestsV1;
 
 import com.handresc1127.pages.homePage;
-import com.handresc1127.utils.BaseTests;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
-public class Task1 extends BaseTests {
+public class Task1 {
 
     @Test(testName = "Cross-Device Elements")
     public void task1() {
