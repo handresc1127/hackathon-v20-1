0e81c9d80cadde266f11cb0c54e943c248348291
handresc1127 <h.andresc1127@gmail.com>
2020-06-23 21:51:18 -0500

    eyes Manager fix to ultrafast grid

--- src/test/java/com/handresc1127/utils/EyesManager.java
+++ src/test/java/com/handresc1127/utils/EyesManager.java
@@ -1,8 +1,15 @@
 package com.handresc1127.utils;
 
 import com.applitools.eyes.BatchInfo;
+import com.applitools.eyes.EyesRunner;
 import com.applitools.eyes.MatchLevel;
+import com.applitools.eyes.RectangleSize;
+import com.applitools.eyes.selenium.BrowserType;
+import com.applitools.eyes.selenium.Configuration;
 import com.applitools.eyes.selenium.Eyes;
+import com.applitools.eyes.visualgrid.model.DeviceName;
+import com.applitools.eyes.visualgrid.model.ScreenOrientation;
+import com.applitools.eyes.visualgrid.services.VisualGridRunner;
 import org.apache.commons.io.IOUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -10,16 +17,35 @@ import org.openqa.selenium.WebDriver;
 import java.nio.charset.StandardCharsets;
 
 public class EyesManager {
-    Eyes eyes;
-    String appName;
-    WebDriver driver;
+    private Eyes eyes;
+    private String appName;
+    private WebDriver driver;
+    private Configuration eyesConfig;
+    private EyesRunner runner;
+    private int concurrentSessions = 10;
 
     public EyesManager(WebDriver driver, String appName) {
         this.driver = driver;
         this.appName = appName;
 
-        eyes = new Eyes();
-        eyes.setApiKey(PropertyLoader.getProperty("APPLITOOLS_API_KEY"));
+        runner = new VisualGridRunner(concurrentSessions);
+        eyesConfig = (Configuration) new Configuration()
+                // Visual Grid configurations
+        .addBrowser(1200, 700, BrowserType.CHROME)
+        .addBrowser(1200, 700, BrowserType.FIREFOX)
+        .addBrowser(1200, 700, BrowserType.EDGE_CHROMIUM)
+        .addBrowser(768, 700, BrowserType.CHROME)
+        .addBrowser(768, 700, BrowserType.FIREFOX)
+        .addBrowser(768, 700, BrowserType.EDGE_CHROMIUM)
+        .addDeviceEmulation(DeviceName.iPhone_X, ScreenOrientation.PORTRAIT)
+                //  ...other configurations
+        .setViewportSize(new RectangleSize(800, 600))
+        .setApiKey(PropertyLoader.getProperty("APPLITOOLS_API_KEY"))
+        .setAppName(appName)
+        ;
+
+        eyes = new Eyes(runner);
+        eyes.setConfiguration(eyesConfig);
     }
 
     public void setBatchName(String batchName) {
@@ -37,6 +63,7 @@ public class EyesManager {
     }
 
     public void validateWindows(String tagWindows){
+        final StackTraceElement[] ste = Thread.currentThread().getStackTrace();
         eyes.open(driver, appName, Thread.currentThread().getStackTrace()[2].getMethodName());
         eyes.checkWindow(tagWindows);
         eyes.close();
@@ -64,6 +91,7 @@ public class EyesManager {
 
     public void abort(){
         eyes.abortIfNotClosed();
+        runner.getAllTestResults(false);
     }
 
     public Eyes getEyes(){
