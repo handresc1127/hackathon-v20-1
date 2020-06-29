169b23e26332e065aa6c43dc837c62777413539b
handresc1127 <h.andresc1127@gmail.com>
2020-06-28 21:48:10 -0500

    add Task2 Traditional Test

--- src/test/java/com/handresc1127/TraditionalTestsV1/Task1.java
+++ src/test/java/com/handresc1127/TraditionalTestsV1/Task1.java
@@ -1,21 +1,35 @@
 package com.handresc1127.TraditionalTestsV1;
 
 import com.handresc1127.pages.homePage;
+import com.handresc1127.utils.BaseTests;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
-public class Task1 {
+public class Task1 extends BaseTests {
+
 
     @Test(testName = "Cross-Device Elements")
     public void task1() {
-        SoftAssert softAssertions = new SoftAssert();
-
         homePage.navigateToV1();
+
+        SoftAssert softAssertions = new SoftAssert();
         softAssertions.assertTrue(homePage.validateFilterVisible());
         softAssertions.assertTrue(homePage.validateSearchBarVisible());
-
+        //TODO
         softAssertions.assertAll();
-
     }
 
+
+    @Test
+    public void task2(){
+        homePage.navigateToV1();
+        homePage.openFilterIfIsNotDisplayed();
+        homePage.filterForBlack();
+
+        SoftAssert softAssertions = new SoftAssert();
+        softAssertions.assertTrue(homePage.validateFilterVisible());
+        softAssertions.assertTrue(homePage.validateSearchBarVisible());
+        //TODO
+        softAssertions.assertAll();
+    }
 }
