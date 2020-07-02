beac39e3e5284133aea65548cc935627118c45ba
handresc1127 <h.andresc1127@gmail.com>
2020-07-02 17:24:33 -0500

    Add new Validations

--- src/test/java/com/handresc1127/pages/homePage.java
+++ src/test/java/com/handresc1127/pages/homePage.java
@@ -10,22 +10,28 @@ public class homePage extends BaseTests {
     static String urlV1="https://demo.applitools.com/gridHackathonV1.html";
     static String urlV2="https://demo.applitools.com/gridHackathonV2.html";
 
+    static By mainMenu = By.id("DIV__mainmenu__15");
+
     static By filterLabel = By.id("ti-filter");
     static By filterBlack = By.id("SPAN__checkmark__107");
+    static By filterBtn = By.id("filterBtn");
+    static By filterPanel = By.id("filter_col");
+    static By filterIcon = By.id("ti-filter");
+    static By openFilter = By.id("A__openfilter__206");
+
     static By searchBarDiv = By.id("DIV__customsear__41");
     static By searchBarInput= By.id("INPUTtext____42");
     static By searchBarIcon= By.id("BUTTONsubmit____43");
     static By searchBtn = By.id("A__btnsearchm__59");
 
-    static By filterBtn = By.id("filterBtn");
-    static By filterPanel = By.id("filter_col");
-    static By openFilter = By.id("A__openfilter__206");
     static By productsGrid = By.id("product_grid");
     static By fistProduct= By.id("product_1");
 
     static By wishListIcon = By.id("A__wishlist__52");
     static By accessLinkIcon = By.id("A__accesslink__56");
     static By cartIcon = By.id("A__cartbt__49");
+    static By cartValue = By.id("STRONG____50");
+
     static By gridViewBtn = By.id("I__tiviewgrid__202");
     static By listViewBtn = By.id("I__tiviewlist__204");
 
@@ -108,17 +114,33 @@ public class homePage extends BaseTests {
         return validateDisplayed(gridViewBtn, "laptop", true);
     }
     public static boolean validateListViewVisible(){
-        return validateDisplayed(gridViewBtn, "laptop", true);
+        return validateDisplayed(listViewBtn, "laptop", true);
     }
+
+    public static boolean validateMainMenuVisible(){
+        return validateDisplayed(mainMenu, "laptop", true);
+    }
+
+    public static boolean validateFilterIconVisible(){
+        return validateDisplayed(filterIcon, "laptop", false);
+    }
+
+
+
 
 
     public static boolean validateSearchBarText() {
         //TODO
+        //Search over 10,000 shoes!
+        String text= driver.findElement(searchBarInput).getAttribute("placeholder");
+        System.out.println("Text: "+text);
         return true;
     }
 
     public static boolean validateCartValue() {
         //TODO
+        String value= driver.findElement(cartValue).getText();
+        System.out.println("Value: "+value);
         return true;
     }
 }
