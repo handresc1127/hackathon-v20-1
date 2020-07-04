7956777c0c9d90ef0301a3f27e590b7c70c6c813
handresc1127 <h.andresc1127@gmail.com>
2020-07-03 22:33:16 -0500

    Add task3 elements

--- src/test/java/com/handresc1127/pages/homePage.java
+++ src/test/java/com/handresc1127/pages/homePage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
+import java.util.List;
+
 public class homePage extends BaseTests {
 
     static String urlV1="https://demo.applitools.com/gridHackathonV1.html";
@@ -12,7 +14,7 @@ public class homePage extends BaseTests {
 
     static By mainMenu = By.id("DIV__mainmenu__15");
 
-    static By filterLabel = By.id("ti-filter");
+    static By filterLabel = By.id("SPAN____208");
     static By filterBlack = By.id("SPAN__checkmark__107");
     static By filterBtn = By.id("filterBtn");
     static By filterPanel = By.id("filter_col");
@@ -34,6 +36,11 @@ public class homePage extends BaseTests {
 
     static By gridViewBtn = By.id("I__tiviewgrid__202");
     static By listViewBtn = By.id("I__tiviewlist__204");
+
+    static By productOnGrid = By.cssSelector("#product_grid .grid_item");
+    static By productHeart = By.cssSelector(".ti-heart");
+    static By productShuffle = By.cssSelector(".ti-control-shuffle");
+    static By productShopping = By.cssSelector(".ti-shopping-cart");
 
     public static void navigateToV1(){
         driver.get(urlV1);
@@ -74,14 +81,77 @@ public class homePage extends BaseTests {
         return hackathonReporter(locator.toString(),element.isDisplayed()==isElementVisible);
     }
 
+    private static boolean validateDisplayed(WebElement element, String deviceRule, boolean isElementVisible){
+        if(!device.toLowerCase().equals(deviceRule.toLowerCase())){
+            isElementVisible=!isElementVisible;
+        }
+        String[] locators = element.toString().split("->");
+        String locator="";
+        boolean hasLocator=false;
+        for(int i=1;i<locators.length;i++){
+            if(hasLocator){
+                locator+=" -> "+locators[i].replace("]","").split(":")[1].trim();
+            }else{
+                locator+=locators[i].replace("]","").trim();
+            }
+            hasLocator=true;
+        }
+        return hackathonReporter(locator,element.isDisplayed()==isElementVisible);
+    }
+
+    private static boolean validateEnabled(WebElement element, Boolean isEnabled){
+        String[] locators = element.toString().split("->");
+        String locator="";
+        boolean hasLocator=false;
+        for(int i=1;i<locators.length;i++){
+            if(hasLocator){
+                locator+=" -> "+locators[i].replace("]","").split(":")[1].trim();
+            }else{
+                locator+=locators[i].replace("]","").trim();
+            }
+            hasLocator=true;
+        }
+        return hackathonReporter(locator,element.isEnabled()==isEnabled);
+    }
+
     private static boolean validateDisplayed(By locator){
         WebElement element = driver.findElement(locator);
         return hackathonReporter(locator.toString(),element.isDisplayed()==true);
     }
+
+    public static boolean validateParentVisible(By locatorPattern, By locatorChild, String deviceRule){
+        List<WebElement> products= driver.findElements(locatorPattern);
+        for (WebElement product:products) {
+            WebElement heart= product.findElement(locatorChild);
+            if(!validateDisplayed(heart, deviceRule, false)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean validateAttribute(By locator,String attribute, String expectedValue){
+        String currentValue= driver.findElement(locator).getAttribute(attribute);
+        return hackathonReporter(locator.toString(),currentValue.equals(expectedValue));
+    }
+
+    private static boolean validateText(By locator, String deviceRule, String valueDevice, String valueNoDevice){
+        String currentValue=driver.findElement(locator).getText();
+        if(device.toLowerCase().equals(deviceRule.toLowerCase())){
+            return hackathonReporter(locator.toString(),currentValue.equals(valueDevice));
+        }else{
+            return hackathonReporter(locator.toString(),currentValue.equals(valueNoDevice));
+        }
+    }
     // ==============================
 
-    public static boolean validateFilterVisible(){
-        return validateDisplayed(filterLabel,"laptop",false);
+    public static boolean validateFilterLabelVisible(){
+        System.out.println(device+" displayed "+driver.findElement(filterLabel).isDisplayed());
+        System.out.println(device+" enabled "+driver.findElement(filterLabel).isEnabled());
+        System.out.println(device+" selected "+driver.findElement(filterLabel).isSelected());
+        System.out.println(device+" location "+driver.findElement(filterLabel).getLocation());
+        System.out.println(device+" display "+driver.findElement(filterLabel).getAttribute("display"));
+        return validateDisplayed(filterLabel,"tablet",true);
     }
 
     public static boolean validateSearchBarDivVisible(){
@@ -110,6 +180,10 @@ public class homePage extends BaseTests {
         return validateDisplayed(cartIcon);
     }
 
+    public static boolean validateCartValueVisible() {
+        return validateDisplayed(cartValue, "laptop", true);
+    }
+
     public static boolean validateGridViewVisible(){
         return validateDisplayed(gridViewBtn, "laptop", true);
     }
@@ -125,22 +199,28 @@ public class homePage extends BaseTests {
         return validateDisplayed(filterIcon, "laptop", false);
     }
 
-
-
-
-
     public static boolean validateSearchBarText() {
-        //TODO
-        //Search over 10,000 shoes!
-        String text= driver.findElement(searchBarInput).getAttribute("placeholder");
-        System.out.println("Text: "+text);
-        return true;
+        return validateAttribute(searchBarInput,"placeholder","Search over 10,000 shoes!");
     }
 
     public static boolean validateCartValue() {
-        //TODO
-        String value= driver.findElement(cartValue).getText();
-        System.out.println("Value: "+value);
-        return true;
+        return validateText(cartValue,"laptop","2","");
     }
+
+    public static boolean validateCountElement(int sizeExpected){
+        List<WebElement> products= driver.findElements(productOnGrid);
+        return hackathonReporter(productOnGrid.toString(),products.size()==sizeExpected);
+    }
+
+    public static boolean validateProductHeartVisible(){
+        return validateParentVisible(productOnGrid,productHeart,"laptop");
+    }
+    public static boolean validateProductShuffleVisible(){
+        return validateParentVisible(productOnGrid,productShuffle,"laptop");
+    }
+    public static boolean validateProductShoppingVisible(){
+        return validateParentVisible(productOnGrid,productShopping,"laptop");
+    }
+
+
 }
