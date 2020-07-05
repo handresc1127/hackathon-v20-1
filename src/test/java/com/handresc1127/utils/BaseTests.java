ee04e7512e804bf6d5ac7f484d21b28fad59744d
handresc1127 <h.andresc1127@gmail.com>
2020-07-04 20:41:59 -0500

    Remove logger information for debug

--- src/test/java/com/handresc1127/utils/BaseTests.java
+++ src/test/java/com/handresc1127/utils/BaseTests.java
@@ -9,10 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.testng.ITestContext;
 import org.testng.annotations.*;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.lang.reflect.Method;
-import java.util.List;
 
 public class BaseTests{
 
@@ -20,7 +17,6 @@ public class BaseTests{
     protected static WebDriver driver;
     protected static WebDriverWait wait;
     protected static EyesManager eyesManager;
-    private static final Logger LOGGER = LoggerFactory.getLogger(BaseTests.class);
     protected static String browser;
     protected static String viewport;
     protected static String device;
@@ -38,12 +34,6 @@ public class BaseTests{
         if(suiteType.toLowerCase().equals("modern")){
             eyesManager = new EyesManager();
         }
-
-        LOGGER.info("Suited Configured...");
-        LOGGER.info("  suite type: " + suiteType);
-        LOGGER.info("  applitools.appName= "+PropertyLoader.getProperty("applitools.appName"));
-        LOGGER.info("  applitools.batchName= "+PropertyLoader.getProperty("applitools.batchName"));
-        LOGGER.info("  applitools.forceFullScreen= "+PropertyLoader.getProperty("applitools.forceFullScreen"));
     }
 
     @Parameters({ "browser", "device" })
@@ -52,17 +42,11 @@ public class BaseTests{
         browser=_browser;
         device=_device;
         testName=thisTest.getName();
-
-        LOGGER.info("Test Configured...");
-        LOGGER.info("  Browser:" + _browser+" Device:"+_device);
-        LOGGER.info("  Test Name: "+thisTest.getName());
     }
 
     @BeforeMethod
     public static void beforeEach(Method method){
         methodName=method.getName();
-        LOGGER.info("Method beginning...");
-        LOGGER.info("  Method name: " + methodName);
         driverManager = DriverManagerFactory.getManager(browser);
         driver=driverManager.getDriver(browser);
 
@@ -76,8 +60,6 @@ public class BaseTests{
         if(suiteType.toLowerCase().equals("modern")){
             eyesManager.setDriver(driver);
         }
-
-        LOGGER.info("  Browser:" + browser+" Device:"+device+" Viewport:"+viewport);
     }
 
     @AfterMethod
@@ -98,59 +80,4 @@ public class BaseTests{
     public static String getTestName() {
         return testName;
     }
-
-    /**
-     * A Helper to print the test result in the following format:
-     * DOM Locator: <DOM Locator>,
-     * Browser: <Browser>,
-     * Viewport: <Width x Height>,
-     * Device <Device type>,
-     * Status: <Pass | Fail>
-     *
-     * @param domLocator              string - DOM ID of the element
-     * @param comparisonResult        boolean - The result of comparing the "Expected" value and the "Actual" value.
-     * @return                        boolean - returns the same comparison result back so that it can be used for further Assertions in the test code.
-     */
-
-    public static boolean hackathonReporter(String domLocator, boolean comparisonResult) {
-        //String testName=Thread.currentThread().getStackTrace()[3].getMethodName();
-        StackTraceElement[] stack= Thread.currentThread().getStackTrace();
-        String last="";
-        String testName="";
-        String lastClassName="";
-        for (StackTraceElement trace:stack){
-            String className=trace.getClassName();
-            if(className.contains("com.handresc1127")){
-                lastClassName=className;
-                testName=last;
-                last=trace.getMethodName();
-            }
-        }
-
-        String fileName=lastClassName.replace("com.handresc1127.","").replace("Tests","-")+"-TestResults.txt";
-
-        try(BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true))){
-            writer.write("Task: " + methodName.replace("task","")
-                    + ", Test Name: " + normalize(testName+", ",32)
-                    + "DOM Locator: " + normalize(domLocator+", ",64)
-                    + "Browser: " +     normalize(browser+", ",9)
-                    + "Viewport: " +    normalize(viewport+", ",10)
-                    + "Device: " +    normalize(device+", ",8)
-                    + "Status: " +    (comparisonResult ? "Pass" : "Fail"));
-            writer.newLine();
-        }catch(Exception e){
-            LOGGER.error("Error writing to report file "+e);
-        }
-        //returns the result so that it can be used for further Assertions in the test code.
-        return comparisonResult;
-    }
-
-    static String normalize(String text, int maxLen){
-        int len=text.length();
-        for (int i=0;i<(maxLen-len);i++){
-            text+=" ";
-        }
-        return text;
-    }
-
 }
