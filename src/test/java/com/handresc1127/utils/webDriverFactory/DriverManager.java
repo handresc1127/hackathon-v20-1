69c66217b319196f781672ea4459d78f7772fbaf
handresc1127 <h.andresc1127@gmail.com>
2020-07-05 15:08:29 -0500

    DriverManager acording os Name

--- src/test/java/com/handresc1127/utils/webDriverFactory/DriverManager.java
+++ src/test/java/com/handresc1127/utils/webDriverFactory/DriverManager.java
@@ -1,5 +1,6 @@
 package com.handresc1127.utils.webDriverFactory;
 
+import com.handresc1127.utils.PropertyLoader;
 import org.openqa.selenium.WebDriver;
 
 import java.awt.*;
@@ -10,6 +11,10 @@ public abstract class DriverManager {
     protected String browser;
     protected String viewport;
     protected String device;
+    protected OS osName = null;
+    public enum OS {
+        WINDOWS, LINUX, MAC
+    };// Operating systems.
 
     public void quitDriver() {
         if (null != driver) {
@@ -19,6 +24,18 @@ public abstract class DriverManager {
     }
 
     public WebDriver getDriver(String browser) {
+        if (osName == null) {
+            String operativeSys = System.getProperty("os.name").toLowerCase();
+            if (operativeSys.contains("win")) {
+                osName = OS.WINDOWS;
+            } else if (operativeSys.contains("nix") || operativeSys.contains("nux")
+                    || operativeSys.contains("aix")) {
+                osName = OS.LINUX;
+            } else if (operativeSys.contains("mac")) {
+                osName = OS.MAC;
+            }
+        }
+
         if (null == driver) {
             createDriver(browser);
         }
@@ -35,6 +52,20 @@ public abstract class DriverManager {
 
     public String getDevice() {
         return device;
+    }
+
+    protected void setDriverFile(String browserFile){
+        if(osName!=null){
+            final String driverKey="webdriver."+browserFile+".driver";
+            String driverValue="driver.os."+browserFile;
+            switch(osName)
+            {
+                case WINDOWS: driverValue= driverValue.replace("os","windows");break;
+                case LINUX: driverValue= driverValue.replace("os","linux");break;
+                case MAC: driverValue= driverValue.replace("os","mac");break;
+            }
+            PropertyLoader.setProperty(driverKey,PropertyLoader.getProperty(driverValue));
+        }
     }
 
     public void setDeviceSize(String device_){
